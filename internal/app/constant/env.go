5de938da72269a04235a859e7d4e15a05a7386d3
Sergey Davydov <dev@sdcollection.com>
2024-02-27 17:37:18 +0300

    env sync

--- internal/app/constant/env.go
+++ internal/app/constant/env.go
@@ -4,6 +4,6 @@ const (
 	EnvNameServerAddress = "ADDRESS"
 	EnvNameDBDSN         = "DATABASE_DSN"
 	EnvNameRedisAddress  = "REDIS_ADDRESS"
-	EnvNameRedisPass     = "REDIS_PASS"
+	EnvNameRedisPass     = "REDIS_PASSWORD"
 	EnvNameJWTSecretKey  = "JWT_SECRET_KEY"
 )
